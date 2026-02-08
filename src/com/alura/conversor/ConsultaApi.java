c9415730183ae912f914aa9ab3b333843c48ebc3
Eckert11 <eckert.baezag@gmail.com>
2026-02-07 22:38:23 -0600

    Implementado cliente HTTP para consumo de la API

---
+++ src/com/alura/conversor/ConsultaApi.java
@@ -0,0 +1,27 @@
+package com.alura.conversor;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConsultaApi {
+
+    public String obtenerDatos(String url) throws IOException, InterruptedException {
+
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .GET()
+                .build();
+
+        HttpResponse<String> response = client.send(
+                request,
+                HttpResponse.BodyHandlers.ofString()
+        );
+
+        return response.body();
+    }
+}
--- src/com/alura/conversor/Principal.java
+++ src/com/alura/conversor/Principal.java
@@ -1,4 +1,13 @@
 package com.alura.conversor;
 
 public class Principal {
+    public static void main(String[] args) throws Exception {
+
+        ConsultaApi consulta = new ConsultaApi();
+
+        String url = "https://v6.exchangerate-api.com/v6/Tu_API_KEY/pair/USD/ARS";
+        String json = consulta.obtenerDatos(url);
+
+        System.out.println(json);
+    }
 }
